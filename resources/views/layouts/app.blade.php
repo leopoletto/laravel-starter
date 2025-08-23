509e29ecf37f1d817fd0e028c1461a06d74c75fa
Leonardo Poletto <lgupoletto@gmail.com>
2025-08-23 01:13:17 -0300

    Replace fonts with Font Lint CDN

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -11,24 +11,28 @@
 
     <script type="application/ld+json">@json(config('seo.schema'))</script>
 
-@foreach(config('seo.opengraph') as $property => $content)
-    <meta property="og:{{$property}}" content="{{ is_callable($content) ? $content() : $content  }}"/>
-@endforeach
+    @foreach(config('seo.opengraph') as $property => $content)
+        <meta property="og:{{$property}}" content="{{ is_callable($content) ? $content() : $content  }}"/>
+    @endforeach
 
-@foreach(config('seo.twitter') as $property => $content)
-    <meta property="twitter:{{$property}}" content="{{ is_callable($content) ? $content() : $content  }}"/>
-@endforeach
+    @foreach(config('seo.twitter') as $property => $content)
+        <meta property="twitter:{{$property}}" content="{{ is_callable($content) ? $content() : $content  }}"/>
+    @endforeach
 
-@foreach(config('seo.icons') as $icon)
-    <link {!! collect($icon)->map(fn($value, $key) => $key . '="'.( is_callable($value) ? $value() : $value ).'"')->join(' ') !!} />
-@endforeach
+    @foreach(config('seo.icons') as $icon)
+        <link {!! collect($icon)->map(fn($value, $key) => $key . '="'.( is_callable($value) ? $value() : $value ).'"')->join(' ') !!} />
+    @endforeach
 
-@foreach(config('seo.meta') as $property => $content)
-    <meta property="{{$property}}" content="{{$content}}"/>
-@endforeach
+    @foreach(config('seo.meta') as $property => $content)
+        <meta property="{{$property}}" content="{{$content}}"/>
+    @endforeach
 
-    <script defer data-domain="wizardcompass.com" src="https://plausible.io/js/script.file-downloads.hash.outbound-links.tagged-events.js"></script>
+    <link rel="preconnect" href="https://cdn.fontlint.com">
+    <link href="https://cdn.fontlint.com/wizardcompass/mozilla-headline.css" rel="stylesheet">
+    <link href="https://cdn.fontlint.com/wizardcompass/inter.css" rel="stylesheet">
 
+        <script defer data-domain="wizardcompass.com"
+                src="https://plausible.io/js/script.file-downloads.hash.outbound-links.tagged-events.js"></script>
     @vite(["resources/js/app.js", "resources/css/app.css"])
 </head>
 <body class="min-h-screen overflow-auto">
